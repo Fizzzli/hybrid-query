f3b2e28ba2a65ff76bafec6e9f670223efbd6686
Fizzzli <f416638654@gmail.com>
2021-01-22 18:00:49 +0800

    init

---
+++ hybrid-query-api/src/test/java/com/fizz/hybrid/query/api/HybridQueryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fizz.hybrid.query.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybridQueryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
